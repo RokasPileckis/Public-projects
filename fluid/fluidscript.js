8d07c4c048a6892d6b4458946da07f61ffe3a83f
RokasPileckis <144928537+RokasPileckis@users.noreply.github.com>
2025-03-25 16:49:22 +0200

    Update fluidscript.js

--- fluid/fluidscript.js
+++ fluid/fluidscript.js
@@ -8,6 +8,7 @@ let width = 300;
 let height = 200;
 let nodes = [];
 let runanimation = false;
+let imgData;
 
 function setupcanvas()
 {
@@ -17,8 +18,8 @@ function setupcanvas()
   canvas.height = height;
 
   context = canvas.getContext('2d', { willReadFrequently: true });
-  //context.drawImage(image, 0, 0);
-  pixeldata = context.getImageData(0, 0, canvas.width, canvas.height);
+  context.globalAlpha = 0.2;
+  
   print("setup")
   
   drawborder();
@@ -94,9 +95,15 @@ function mouseclick()
     nodes.push({
       posx: x,
       posy: y,
-      velx: 0,
+      velx: (Math.random()-0.5)*10,
       vely: 0,
     });
+    //print("---");
+    //let pixel = 220;
+    //let imgData = context.getImageData(x-5, y-5, 10, 10);
+    //let imgData = context.getImageData(x, y, 1, 1);
+    //print(imgData.data[3]);
+    
   }
   
 }
@@ -105,20 +112,12 @@ function drawfill(x, y, color, size)
   context.fillStyle = color;
   context.fillRect(x-size/2, y-size/2, size, size);
 }
-function getcolor(index)
-{
-  return pixeldata.data[index]*1000000 + pixeldata.data[index+1]*1000 + pixeldata.data[index+2];
-}
-function getcolorpixel(x, y)
-{
-  return getcolor((y * width + x) * 4);
-}
 function b2()
 {
   runanimation = true;
   animation();
 }
-function inbounds(x, y, id)
+function inbounds(x, y, id)//checks if mouse click is in canvas
 {
   let element = document.getElementById(id);
   let rect = element.getBoundingClientRect();
@@ -133,24 +132,98 @@ function b3()
 {
   runanimation = false;
 }
+function b1()
+{
+  calculatevelocity();
+}
 function drawborder()
 {
   context.strokeRect(0, 0, width, height);
 }
 function calculatevelocity()
 {
-  let deltatime = 0.1;
+  let deltatime = 0.05;
   let gravity = 9.81;
-  let airresistance = 0;
+  let airresistance = 0.99;
+  let atractionforce = 0;
+  let repulsionforce = 100;
+  //print("---");
   for(let i = 0 ; i < nodes.length ; i++)
   {
+    //print("---");
+    //print("node " + i);
     nodes[i].vely += gravity*deltatime;
     nodes[i].posy += nodes[i].vely*deltatime;
     nodes[i].posx += nodes[i].velx*deltatime;
-    if(nodes[i].posy > height) //bounce off bottom
+    
+    //add nodes colisions
+    //check if see color, exept its own, find direction and bounce off
+    imgData = context.getImageData(nodes[i].posx-2, nodes[i].posy-2, 5, 5);
+    /*
+    5x5 grid
+     0  4  8 12 16
+    20 24 28 32 36
+    40 44 48 52 56
+    60 64 68 72 76
+    80 84 88 92 96
+    */
+    
+    
+    if(imgData.data[51] > 60)
     {
-      nodes[i].vely *= -1;
-      nodes[i].posy = height - (nodes[i].posy - height);
+      if(imgData.data[3] > 60)//top left
+      {
+        nodes[i].vely += repulsionforce*deltatime;
+        nodes[i].velx += repulsionforce*deltatime;
+      }
+      if(imgData.data[19] > 60)//top right
+      {
+        nodes[i].vely += repulsionforce*deltatime;
+        nodes[i].velx -= repulsionforce*deltatime;
+      }
+      
+      if(imgData.data[83] > 60)//bottom left
+      {
+        nodes[i].vely -= repulsionforce*deltatime;
+        nodes[i].velx += repulsionforce*deltatime;
+      }
+      
+      if(imgData.data[99] > 60)//bottom right
+      {
+        nodes[i].vely -= repulsionforce*deltatime;
+        nodes[i].velx -= repulsionforce*deltatime;
+      }
+    }
+    
+    //air resisance
+    nodes[i].vely *= airresistance;
+    nodes[i].velx *= airresistance;
+    
+    
+    
+    if(nodes[i].posy > height-5) //bounce off bottom
+    {
+      //nodes[i].vely *= -1;
+      //nodes[i].posy = height - (nodes[i].posy - height);
+      nodes[i].vely -= repulsionforce*deltatime;
+    }
+    if(nodes[i].posy < 5) //bounce off top
+    {
+      //nodes[i].vely *= -1;
+      //nodes[i].posy *= -1;
+      nodes[i].vely += repulsionforce*deltatime;
+    }
+    if(nodes[i].posx < 5) //bounce off left
+    {
+      //nodes[i].velx *= -1;
+      //nodes[i].posx *= -1;
+      nodes[i].velx += repulsionforce*deltatime;
+    }
+    if(nodes[i].posx > width-5) //bounce off right
+    {
+      //nodes[i].velx *= -1;
+      //nodes[i].posx = width - (nodes[i].posx - width);
+      nodes[i].velx -= repulsionforce*deltatime;
     }
   }
   
@@ -158,21 +231,23 @@ function calculatevelocity()
 }
 function drawnodes()
 {
+  context.globalAlpha = 1;
   clearcanvas();
+  context.globalAlpha = 0.2;
   drawborder();
+
   
   let x, y;
   for(let i = 0 ; i < nodes.length ; i++)
   {
     x = nodes[i].posx;
-    y =nodes[i].posy;
+    y = nodes[i].posy;
     drawfill(x, y, "blue", 10);
   }
 }
 function clearcanvas()
 {
-  context.fillStyle = "white";
-  context.fillRect(0, 0, width, height);
+  context.clearRect(0, 0, width, height);
 }
 async function animation()
 {
@@ -182,7 +257,10 @@ async function animation()
     await sleep(0);
   }
 }
-
+function generatenodes(amount)
+{
+  
+}
 
 
 
