3b6cff67be769ab6496320558cdeb963f4caa1d2
RokasPileckis <144928537+RokasPileckis@users.noreply.github.com>
2025-03-28 16:51:23 +0200

    Update fluidscript.js

--- fluid/fluidscript.js
+++ fluid/fluidscript.js
@@ -9,6 +9,8 @@ let height = 200;
 let nodes = [];
 let runanimation = false;
 let imgData;
+let densitygrid = [];
+let gridsize = 10;
 
 function setupcanvas()
 {
@@ -23,6 +25,7 @@ function setupcanvas()
   print("setup")
   
   drawborder();
+  generatedensitygrid();
 
 }
 function drawpixel(x, y, color)
@@ -84,13 +87,12 @@ function mouseclick()
 {
   let x = event.pageX;
   let y = event.pageY;
-  //print("click: " + x + " " + y);
   
   if(inbounds(x, y, "canvas"))
   {
     x = event.offsetX;
     y = event.offsetY;
-    //print("in bounds");
+
     drawfill(x, y, "blue", 10);
     nodes.push({
       posx: x,
@@ -98,11 +100,6 @@ function mouseclick()
       velx: (Math.random()-0.5)*10,
       vely: 0,
     });
-    //print("---");
-    //let pixel = 220;
-    //let imgData = context.getImageData(x-5, y-5, 10, 10);
-    //let imgData = context.getImageData(x, y, 1, 1);
-    //print(imgData.data[3]);
     
   }
   
@@ -147,18 +144,21 @@ function calculatevelocity()
   let airresistance = 0.99;
   let atractionforce = 0;
   let repulsionforce = 100;
+  let x, y;
+  let n = [9];
+  let d = [9];
   //print("---");
   for(let i = 0 ; i < nodes.length ; i++)
   {
+    repulsionforce = 50;
     //print("---");
     //print("node " + i);
-    nodes[i].vely += gravity*deltatime;
-    nodes[i].posy += nodes[i].vely*deltatime;
-    nodes[i].posx += nodes[i].velx*deltatime;
+    //nodes[i].vely += gravity*deltatime;
+    
     
     //add nodes colisions
     //check if see color, exept its own, find direction and bounce off
-    imgData = context.getImageData(nodes[i].posx-2, nodes[i].posy-2, 5, 5);
+    //imgData = context.getImageData(nodes[i].posx-2, nodes[i].posy-2, 5, 5);
     /*
     5x5 grid
      0  4  8 12 16
@@ -168,7 +168,7 @@ function calculatevelocity()
     80 84 88 92 96
     */
     
-    
+    /*
     if(imgData.data[51] > 60)
     {
       if(imgData.data[3] > 60)//top left
@@ -194,13 +194,36 @@ function calculatevelocity()
         nodes[i].velx -= repulsionforce*deltatime;
       }
     }
+    */
+    /*
+    if(imgData.data[11] > 60)
+      nodes[i].vely += repulsionforce*deltatime;
+      
+    if(imgData.data[91] > 60)
+      nodes[i].vely -= repulsionforce*deltatime;
+    
+    if(imgData.data[43] > 60)
+      nodes[i].velx += repulsionforce*deltatime;
+      
+    if(imgData.data[59] > 60)
+      nodes[i].velx -= repulsionforce*deltatime;
+     
+    */ 
+    //if(imgData.data[43] > 60 && imgData.data[59] > 60)
+      //nodes[i].vely -= repulsionforce*deltatime;
+    
+    
+    
+    
+    
+    
     
     //air resisance
-    nodes[i].vely *= airresistance;
-    nodes[i].velx *= airresistance;
+    //nodes[i].vely *= airresistance;
+    //nodes[i].velx *= airresistance;
     
-    
-    
+    /*
+    repulsionforce*=2;
     if(nodes[i].posy > height-5) //bounce off bottom
     {
       //nodes[i].vely *= -1;
@@ -225,7 +248,122 @@ function calculatevelocity()
       //nodes[i].posx = width - (nodes[i].posx - width);
       nodes[i].velx -= repulsionforce*deltatime;
     }
+    */
+    
+    x = nodes[i].posx;
+    y = nodes[i].posy;
+    x1 = Math.floor(x / gridsize);
+    y1 = Math.floor(y / gridsize);
+    
+    
+    
+    n[0] = (width/gridsize)*(y1-1) + (x1-1);
+    n[1] = (width/gridsize)*(y1-1) + (x1);
+    n[2] = (width/gridsize)*(y1-1) + (x1+1);
+    n[3] = (width/gridsize)*(y1) + (x1-1);
+    n[4] = (width/gridsize)*(y1) + (x1);
+    n[5] = (width/gridsize)*(y1) + (x1+1);
+    n[6] = (width/gridsize)*(y1+1) + (x1-1);
+    n[7] = (width/gridsize)*(y1+1) + (x1);
+    n[8] = (width/gridsize)*(y1+1) + (x1+1);
+ 
+    if(x1 == 0)
+    {
+      n[0] = -1;
+      n[3] = -1;
+      n[6] = -1;
+    }
+    if(x1 == (width/gridsize)-1)
+    {
+      n[2] = -1;
+      n[5] = -1;
+      n[8] = -1;
+    }
+    if(y1 == 0)
+    {
+      n[0] = -1;
+      n[1] = -1;
+      n[2] = -1;
+    }
+    if(y1 == (height/gridsize)-1)
+    {
+      n[6] = -1;
+      n[7] = -1;
+      n[8] = -1;
+    }
+    
+    print(x + " " + y);
+    print(x1 + " " + y1);
+    
+    for(let i = 0 ; i < 9 ; i++)
+    {
+      d[i] = getdensity(n[i]);
+      print(n[i]);
+    }
+      
+      
+    let min, minid;
+    min = 0;
+    minid = 0;
+    for(let i = 1 ; i < 9 ; i++)
+    {
+      if(min < d[i] && d[i] >= 0)
+      {
+        min = d[i];
+        minid = i;
+      }
+    }
+    
+    //n0 n1 n2
+    //n3 n4 n5
+    //n6 n7 n8
+    
+    switch (minid) {
+      case '0':
+        nodes[i].vely -= repulsionforce*deltatime;
+        nodes[i].velx -= repulsionforce*deltatime;
+        break;
+      case '1':
+        nodes[i].vely -= repulsionforce*deltatime;
+        break;
+      case '2':
+        nodes[i].vely -= repulsionforce*deltatime;
+        nodes[i].velx += repulsionforce*deltatime;
+        break;
+      case '3':
+        
+        nodes[i].velx -= repulsionforce*deltatime;
+        break;
+      case '4':
+        
+        
+        break;
+      case '5':
+        
+        nodes[i].velx += repulsionforce*deltatime;
+        break;
+      case '6':
+        nodes[i].vely += repulsionforce*deltatime;
+        nodes[i].velx -= repulsionforce*deltatime;
+        break;
+      case '7':
+        nodes[i].vely += repulsionforce*deltatime;
+        
+        break;
+      case '8':
+        nodes[i].vely += repulsionforce*deltatime;
+        nodes[i].velx += repulsionforce*deltatime;
+        break;
+      
+    }
+    
   }
+  for(let i = 0 ; i < nodes.length ; i++)
+  {
+    nodes[i].posy += nodes[i].vely*deltatime;
+    nodes[i].posx += nodes[i].velx*deltatime;
+  }
+    
   
   drawnodes();
 }
@@ -259,8 +397,64 @@ async function animation()
 }
 function generatenodes(amount)
 {
-  
+  let spacing = 15;
+  for(let i = 0 ; i < Math.sqrt(amount) ; i++)
+    for(let o = 0 ; o < Math.sqrt(amount) ; o++)
+    {
+      drawfill(i * spacing, o, "blue", 10);
+      nodes.push({
+        posx: i * spacing,
+        posy: o * spacing,
+        velx: (Math.random()-0.5)*10,
+        vely: 0,
+      });
+    }
 }
+function b4()
+{
+  generatenodes(81);
+}
+function calculatedensity()
+{
+  let x, y;
+  imgData = context.getImageData(0, 0, width, height);
+  for(let i = 0 ; i < densitygrid.length ; i++)
+  {
+    x = densitygrid[i].posx;
+    y = densitygrid[i].posy;
+    densitygrid[i].density = imgData.data[x*y*4+3];
+    print(densitygrid[i].density);
+    //drawfill(densitygrid[i].posx, densitygrid[i].posy, "red", 1);
+  }
+}
+function b5()
+{
+  //generatedensitygrid();
+  calculatedensity();
+}
+function generatedensitygrid()
+{
+  //let gridsize = 10;
+  for(let i = 0 ; i < width*height/gridsize/gridsize ; i++)
+  {
+    densitygrid.push({
+      posx: ((i * gridsize) % width)+gridsize/2,
+      posy: (Math.floor((i*gridsize) / width)*gridsize)+gridsize/2,
+      density: 0,
+    });
+    //print(densitygrid[i].posx + " " + densitygrid[i].posy);
+  }
+  //print(width*height/gridsize/gridsize);
+}
+function getdensity(index)
+{
+  if(index == -1)return -1;
+  else return densitygrid[index].density;
+}
+
+
+
+
 
 
 
